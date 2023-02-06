af2f5eafdfb9aa8e4f8b011e4f70e1d5cb19572a
mr-dargi <dargimohammadreza@gmail.com>
2023-02-07 00:25:31 +0330

    redeclear a code a make it cleaner and beter

--- js/main.js
+++ js/main.js
@@ -1,75 +1,85 @@
-const semicircles = document.querySelectorAll(".semicircle");
-let [getHours, getMinutes, getSeconds] = [0, 0, 0];
-let [hours, minutes, seconds] = [0, 0, 0];
-let futureTime;
-let int = null;
-let setTime;
-let currentTime;
+// recive semicircle for changing css value
+let semicircles = document.querySelectorAll(".semicircle")
 
-const getData = document.getElementById("getData").addEventListener("click", () => {
+// create variable for user Inputs
+let [getHours, getMinutes, getSeconds] = [0,0,0]; 
+
+// Recive buttons
+const startTimer = document.getElementById("startTimer");
+const stopTimer = document.getElementById("stopTimer");
+
+// Recive timer document
+const timer = document.getElementById("timer");
+
+// Make variable for put setInterval
+let int = null;
+
+
+
+// create future Time variable 
+let getTime = Date.now();
+let makeTime;
+let futureTime;
+
+// make function for recive buttons events
+startTimer.addEventListener("click", () => {
+    // recive user input 
     getHours = document.getElementById("getHours").value;
     getMinutes = document.getElementById("getMinutes").value;
     getSeconds = document.getElementById("getSeconds").value;
 
-    hours = getHours * 3600000;
-    minutes = getMinutes * 60000;
-    seconds = getSeconds * 1000;
+    console.log(getSeconds)
+    // changing time to miliseconds
+    const hours = getHours * 3600000;
+    const minutes = getMinutes * 60000;
+    const seconds = getSeconds * 1000;
 
-    let startTime = Date.now();
-    setTime = hours + minutes + seconds;
-    futureTime = startTime + setTime;
-    currentTime = Date.now();
-    function startTimer() {
-        if(int !== null) {
-            clearInterval(int);
-        }
-    
-        int = setInterval(countDownTimer, 10);
+    // redclear future time
+    makeTime = hours + minutes + seconds;
+    futureTime = makeTime + getTime;
+
+    console.log(seconds);
+    // make interval
+    if(int !== null) {
+        clearInterval(int);
     }
-
-    startTimer();
+    int = setInterval(countDownTimer, 10)
 })
 
+stopTimer.addEventListener("click", () => {
+    clearInterval(int)
+})
 
- 
-
-// const startTimer = document.getElementById("startTimer");
-const stopTimer = document.getElementById("stopTimer");
-
-
-
-// let timerLoop = setInterval(countDownTimer, 10);
-
-
+// make Function to count time 
 const countDownTimer = () => {
-    currentTime+= 10;
-    let remainingTime = futureTime - currentTime;
-    let angle = (remainingTime / setTime) * 360;
-    console.log(1);
+    getTime+= 10;
+
+    const remainingTime = futureTime - getTime;
+    const hrs = Math.floor((remainingTime / 36000000) % 24);
+    const mins = Math.floor((remainingTime / 60000) % 60);
+    const secs = Math.floor((remainingTime / 1000) % 60);
+
+    timer.innerHTML = `${hrs}:${mins}:${secs}`;
+
+    // make progress indicator
+    let angle = (remainingTime / makeTime) * 360;
+    
     if(angle > 180) {
         semicircles[2].style.display = "none";
         semicircles[0].style.transform = "rotate(180deg)";
         semicircles[1].style.transform = `rotate(${angle}deg)`;
-        console.log(2)
     } else {
         semicircles[2].style.display = "block";
         semicircles[0].style.transform = `rotate(${angle}deg)`;
         semicircles[1].style.transform = `rotate(${angle}deg)`;
-        console.log(3);
     }
-
 
     if(remainingTime < 0) {
         clearInterval(int);
+        timer.innerHTML = "00:00:00";
+        semicircles[2].style.display = "none";
+        semicircles[0].style.display = "none";
+        semicircles[1].style.display = "none";
     }
-
 }
 
-
-
-
-
-stopTimer.addEventListener("click", () => {
-    clearInterval(int);
-
-})
